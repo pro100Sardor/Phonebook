7f168a5209f18f43ef97c34d66233497678602f4
pro100Sardor <sardorbek.juraqulov@gmail.com>
2020-11-15 23:56:11 +0500

    WIP search is complated

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
+var elSiteMain = document.querySelector('#siteMain');
+// var elPhonebookInfo = document.querySelector('#phonebookInfo');
+var elPhonebookTable = elSiteMain.querySelector('#phonebookTable');
+var elInitialContactText = elSiteMain.querySelector('#initialContactText');
+
 var elPhoneBookForm = document.querySelector('#phoneBookForm');
 var elNumberOwnerFirstname = elPhoneBookForm.querySelector('#numberOwnerFirstname');
 var elNumberOwnerLastname = elPhoneBookForm.querySelector('#numberOwnerLastname');
 var elPhoneNumber = elPhoneBookForm.querySelector('#phoneNumber');
 var elNumberOwnerConnection = elPhoneBookForm.querySelector('#numberOwnerConnection');
-var elPhonebookInfo = document.querySelector('#phonebookInfo');
 
 function createPhonebook (firstName, lastName, phoneNumber, connection) {
   return {
@@ -19,21 +23,32 @@ elPhoneBookForm.addEventListener('submit', function(evt) {
 
   var numberOwnerFirstname = elNumberOwnerFirstname.value.trim();
   // numberOwnerFirstname = numberOwnerFirstname.charAt(0).toUpperCase() + numberOwnerFirstname.slice(1);
-
   var numberOwnerLastname = elNumberOwnerLastname.value.trim();
   var phoneNumber = elPhoneNumber.value.trim();
   var numberOwnerConnection = elNumberOwnerConnection.value.trim();
+
+  var newTr = document.createElement('tr');
 
   var phonebook = [];
   var phonebookItem = createPhonebook(numberOwnerFirstname, numberOwnerLastname, phoneNumber, numberOwnerConnection);
   phonebook.push(phonebookItem);
 
-  // debugger;
+  elInitialContactText.remove();
 
   for (var i = 0; i < phonebook.length; i++) {
     for (var phonebookItemm in phonebook[i]) {
-      elPhonebookInfo.textContent += phonebook[i][phonebookItemm];
+      var newTd = document.createElement('td');
+      newTd.textContent = phonebook[i][phonebookItemm];
+      newTr.appendChild(newTd);
     }
   }
-  console.log(phonebook);
+
+
+  elPhonebookTable.appendChild(newTr);
+
+  // numberOwnerFirstname.value = '';
+  // numberOwnerLastname.value = '';
+  // phoneNumber.value = '';
+  // numberOwnerConnection.value = '';
+
 });
